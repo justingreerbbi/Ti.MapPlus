89f29b044d9a277b58a37dab9a0bf4726f1e0cd9
Justin Greer <justin@justin-greer.com>
2020-08-05 23:43:56 -0400

    First Update for 9+ GA
    This will now build but I am unsure if it will work yet. I am not hopfull yet but it is nothing to cruise through the code base and update as neeed.

---
+++ ios/build/TiMap.build/Release-iphoneos/TiMap.build/Objects-normal/arm64/TiMapModuleAssets.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/justin/Documents/Appcelerator_Studio_Workspace/TiMap/ios/TiMap_Prefix.pch \
+  /Users/justin/Documents/Appcelerator_Studio_Workspace/TiMap/ios/Classes/TiMapModuleAssets.m \
+  /Users/justin/Documents/Appcelerator_Studio_Workspace/TiMap/ios/Classes/TiMapModuleAssets.h
---
+++ ios/build/TiMap.build/Release-iphoneos/TiMap.build/Objects-normal/armv7/TiMapModule.d
@@ -0,0 +1,90 @@
+dependencies: \
+  /Users/justin/Documents/Appcelerator_Studio_Workspace/TiMap/ios/TiMap_Prefix.pch \
+  /Users/justin/Documents/Appcelerator_Studio_Workspace/TiMap/ios/Classes/TiMapModule.m \
+  /Users/justin/Documents/Appcelerator_Studio_Workspace/TiMap/ios/Classes/TiMapModule.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/include/TiModule.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Modules/module.modulemap \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/Webcolor.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/UIImage+RoundedCorner.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/UIImage+Resize.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/UIImage+Alpha.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiUIiOSTransitionAnimationProxy.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiViewController.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiTab.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiWindowProxy.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiUIWindowProxy.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiUIWindow.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiUIViewProxy.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiToolbar.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiTabGroup.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiLogServer.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiLocale.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiLayoutQueue.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiStreamProxy.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiFilesystemFileStreamProxy.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiFilesystemFileProxy.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiExceptionHandler.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiDefines.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiComplexValue.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiBlob.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiViewTemplate.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiGradient.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiPoint.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/ListenerEntry.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiAnimation.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/LayoutConstraint.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiUIView.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiRect.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiViewProxy.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiControllerProtocols.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiRootViewController.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiStylesheet.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiHost.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/WebFont.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiFile.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiColor.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiBuffer.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiUtils.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiModule.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/Bridge.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/KrollBridge.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiApp.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/Ti3DMatrix.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiBindingRunLoop.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/KrollObject.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/KrollWrapper.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/KrollCallback.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiProxy.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/Ti2DMatrix.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/SBJSON.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/OperationQueue.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/NSData+Additions.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/Mimetypes.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/JSValue+Addons.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiDimension.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/ImageLoader.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/APSHTTPResponse.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/APSHTTPRequest.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/APSHTTPPostForm.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/APSHTTPHelper.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/APSHTTPClient.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/APSAnalytics.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/KrollContext.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiEvaluator.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/ObjcProxy.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/APIModule.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiPublicAPI.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiThreading.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiSharedConfig.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TiBase.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/Frameworks/TitaniumKit.framework/Headers/TitaniumKit.h \
+  /Users/justin/Documents/Appcelerator_Studio_Workspace/TiMap/ios/Classes/TiMapViewProxy.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/include/TiViewProxy.h \
+  /Users/justin/Documents/Appcelerator_Studio_Workspace/TiMap/ios/Classes/TiMapAnnotationProxy.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/include/TiBase.h \
+  /Users/justin/Documents/Appcelerator_Studio_Workspace/TiMap/ios/Classes/TiMapPolygonProxy.h \
+  /Users/justin/Documents/Appcelerator_Studio_Workspace/TiMap/ios/Classes/TiMapCircleProxy.h \
+  /Users/justin/Documents/Appcelerator_Studio_Workspace/TiMap/ios/Classes/TiMapPolylineProxy.h \
+  /Users/justin/Documents/Appcelerator_Studio_Workspace/TiMap/ios/Classes/TiMapIOS7ViewProxy.h \
+  /Users/justin/Documents/Appcelerator_Studio_Workspace/TiMap/ios/Classes/TiMapCameraProxy.h \
+  /Users/justin/Library/Application\ Support/Titanium/mobilesdk/osx/9.0.3.GA/iphone/include/TiProxy.h
